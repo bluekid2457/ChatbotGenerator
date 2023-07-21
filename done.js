23976381d3002a9baad3f2a8b6049a66e553634f
Bluekid <bluekid2457@gmail.com>
2023-07-21 10:59:35 +0530

    working

---
+++ done.js
@@ -0,0 +1,13 @@
+const chatbotTest = () => {
+    domain = localStorage.getItem("domain");
+    message = document.getElementById("chat_input").value
+    console.log(domain+": domain")
+    // console.log(username);
+    fetch("https://d7quvtbtqi.execute-api.us-west-2.amazonaws.com/default/testFunc1?company="+domain +"&uinput="+message).then(res => res.text()).then(data => {
+        text = document.getElementById("chat_response");
+        text.innerHTML = data;
+    }).catch(() => {
+        text = document.getElementById("chat_response");
+        text.innerHTML = "Error";
+})    
+}
