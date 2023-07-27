89ee7fa52554e6b7ba42094848ceae5f007b1918
Bluekid <bluekid2457@gmail.com>
2023-07-27 21:59:01 +0530

    minor

--- chatbots.js
+++ chatbots.js
@@ -1,5 +1,6 @@
 // basically add a function that checks for that chatbots they alr have from s3 bucket and not jsut from previous page and use that later
 link_local = "http://127.0.0.1:5503/create2.html"
+
 text = document.getElementById("existingchatbot");
 console.log(localStorage.getItem("domain"));
 create_link_local = link_local;
@@ -7,6 +8,6 @@ create_link = "https://bluekid2457.github.io/ChatbotGeneratorTest/create2.html";
 text.innerHTML = localStorage.getItem("domain");
 
 const navCreate = () => {
-    window.location=link_local;
+    window.location=localStorage.getItem("links")+"create2.html";
     // window.location=create_link;
 }
